26c30a5835b74e5549cb2e21724a92870ff366a1
Evan <evangarreth@gmail.com>
2023-03-10 20:15:24 -0600

    add line movement

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -61,16 +61,24 @@ vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', { desc = 'move to lower buffer
 vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { desc = 'move to upper buffer'})
 vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { desc = 'move to right buffer'})
 
+-- move lines
+vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
 -- Bufferline
 require('bufferline').setup {
     options = {
         diagnostics = 'nvim_lsp',
         offsets = {
             {
-            filetype = 'NvimTree',
-            text = 'File Explorer',
-            text_align = 'left',
-            separator = true
+                filetype = 'NvimTree',
+                text = 'File Explorer',
+                text_align = 'left',
+                separator = true
             }
         },
     }
