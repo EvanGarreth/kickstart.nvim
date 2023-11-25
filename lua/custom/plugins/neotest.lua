e3103446ab6ed96fa208af03ec6ebb37c7df9095
Evan Campbell <evangarreth@gmail.com>
2023-11-24 20:46:54 -0600

    add neotest & trouble plugins

---
+++ lua/custom/plugins/neotest.lua
@@ -0,0 +1,9 @@
+return {
+	-- Framework for unit testing
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "Issafalcon/neotest-dotnet",
+    },
+}
