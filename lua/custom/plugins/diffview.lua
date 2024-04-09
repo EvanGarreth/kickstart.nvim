08798218dd439f9aa8b04517b14f664c18e9e958
Evan <evangarreth@gmail.com>
2024-04-09 11:08:11 -0500

    add close mapping for diffview

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,4 +1,29 @@
 return {
-	'sindrets/diffview.nvim',
-	lazy=false
+  'sindrets/diffview.nvim',
+  lazy = false,
+  opts = {
+    keymaps = {
+      view = {
+        { 'n', '<leader>q', '<Cmd>DiffviewClose<CR>', { desc = 'Close Diffview' } },
+      },
+      diff1 = {
+        { 'n', '<leader>q', '<Cmd>DiffviewClose<CR>', { desc = 'Close Diffview' } },
+      },
+      diff2 = {
+        { 'n', '<leader>q', '<Cmd>DiffviewClose<CR>', { desc = 'Close Diffview' } },
+      },
+      diff3 = {
+        { 'n', '<leader>q', '<Cmd>DiffviewClose<CR>', { desc = 'Close Diffview' } },
+      },
+      diff4 = {
+        { 'n', '<leader>q', '<Cmd>DiffviewClose<CR>', { desc = 'Close Diffview' } },
+      },
+      file_panel = {
+        { 'n', '<leader>q', '<Cmd>DiffviewClose<CR>', { desc = 'Close Diffview' } },
+      },
+      file_history_panel = {
+        { 'n', '<leader>q', '<Cmd>DiffviewClose<CR>', { desc = 'Close Diffview' } },
+      },
+    },
+  },
 }
