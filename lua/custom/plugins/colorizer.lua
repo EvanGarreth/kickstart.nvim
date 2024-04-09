6b053d3cf17f3ad6506c05dcaa995e5a81908065
Evan <evangarreth@gmail.com>
2024-04-09 11:24:37 -0500

    add colorizer file/buffer exclusions

--- lua/custom/plugins/colorizer.lua
+++ lua/custom/plugins/colorizer.lua
@@ -1,14 +1,23 @@
 return {
-	'nvchad/nvim-colorizer.lua',
-	lazy = false,
-	opts = {
-		user_default_options = {
-			sass = { enable = false, parsers = { "css" }, }, -- Enable sass colors
-			mode = "virtualtext",
-			virtualtext = "■",
-		}
-	}
-	-- config = function()
-	-- 	require("colorizer").setup()
-	-- end
+  'nvchad/nvim-colorizer.lua',
+  lazy = false,
+  opts = {
+    user_default_options = {
+      sass = { enable = false, parsers = { 'css' } }, -- Enable sass colors
+      mode = 'virtualtext',
+      virtualtext = '■',
+    },
+    filetypes = {
+      '*',
+      '!toggleterm',
+      '!mason',
+    },
+    buftypes = {
+      '*',
+      -- exclude prompt and popup buftypes from highlight
+      '!prompt',
+      '!popup',
+      '!terminal',
+    },
+  },
 }
