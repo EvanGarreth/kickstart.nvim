7b02d06f16930bd1409a805d36f446cafec049da
Evan Campbell <evan.garreth@gmail.com>
2024-12-01 16:44:18 -0600

    update neotest; update u/d mappings for cmp

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -1,9 +1,19 @@
 return {
-	-- Framework for unit testing
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "Issafalcon/neotest-dotnet",
-    },
+  -- Framework for unit testing
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'Issafalcon/neotest-dotnet',
+    'rouge8/neotest-rust',
+  },
+  config = function()
+    local neotest = require 'neotest'
+    neotest.setup {
+      adapters = {
+        require 'neotest-dotnet',
+        require 'neotest-rust',
+      },
+    }
+  end,
 }
