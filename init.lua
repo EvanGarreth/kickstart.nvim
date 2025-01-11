5319a295f86bac40500f018a8033884b4ce65cfd
Evan Campbell <evan.garreth@gmail.com>
2025-01-11 17:43:09 -0600

    fix gi mapping

--- init.lua
+++ init.lua
@@ -411,7 +411,7 @@ require('lazy').setup({
           end
 
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
           map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
           map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
           map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
