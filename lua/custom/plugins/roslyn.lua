8ec73c4cd6f04bacab45d28e2062fff802c89681
Evan Campbell <evan.garreth@gmail.com>
2025-02-15 16:13:28 -0600

    update roslyn.lua

--- lua/custom/plugins/roslyn.lua
+++ lua/custom/plugins/roslyn.lua
@@ -2,10 +2,6 @@ return {
   'seblj/roslyn.nvim',
   ft = { 'cs', 'sln', 'fs', 'cshtml' },
   opts = {
-    exe = {
-      'dotnet',
-      vim.fs.joinpath(vim.fn.stdpath 'data', 'roslyn', 'Microsoft.CodeAnalysis.LanguageServer.dll'),
-    },
     filewatching = true,
   },
 }
