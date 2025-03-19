407678b4fe2412cb5ea2660d104044e05b152527
Evan Campbell <evan.garreth@gmail.com>
2025-03-18 20:26:02 -0500

    quit neovim if snacks is the only window

--- init.lua
+++ init.lua
@@ -840,5 +840,27 @@ end
 
 set_shada_file()
 
+vim.api.nvim_create_autocmd('QuitPre', {
+  callback = function()
+    local snacks_windows = {}
+    local floating_windows = {}
+    local windows = vim.api.nvim_list_wins()
+    for _, w in ipairs(windows) do
+      local filetype = vim.api.nvim_get_option_value('filetype', { buf = vim.api.nvim_win_get_buf(w) })
+      if filetype:match 'snacks_' ~= nil then
+        table.insert(snacks_windows, w)
+      elseif vim.api.nvim_win_get_config(w).relative ~= '' then
+        table.insert(floating_windows, w)
+      end
+    end
+    if 1 == #windows - #floating_windows - #snacks_windows then
+      -- Should quit, so we close all Snacks windows.
+      for _, w in ipairs(snacks_windows) do
+        vim.api.nvim_win_close(w, true)
+      end
+    end
+  end,
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
