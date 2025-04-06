fd408b7f0da711e073bd256ceee3778f1c588237
Evan Campbell <evan.garreth@gmail.com>
2025-04-06 11:50:46 -0500

    update neotest again

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -9,12 +9,45 @@ return {
     'nvim-neotest/nvim-nio',
   },
   opts = {
+    status = { virtual_text = true },
     output = { open_on_run = true },
     adapters = {
       'neotest-dotnet',
       'neotest-rust',
     },
   },
+  config = function()
+    if opts.adapters then
+      local adapters = {}
+      for name, config in pairs(opts.adapters or {}) do
+        if type(name) == 'number' then
+          if type(config) == 'string' then
+            config = require(config)
+          end
+          adapters[#adapters + 1] = config
+        elseif config ~= false then
+          local adapter = require(name)
+          if type(config) == 'table' and not vim.tbl_isempty(config) then
+            local meta = getmetatable(adapter)
+            if adapter.setup then
+              adapter.setup(config)
+            elseif adapter.adapter then
+              adapter.adapter(config)
+              adapter = adapter.adapter
+            elseif meta and meta.__call then
+              adapter = adapter(config)
+            else
+              error('Adapter ' .. name .. ' does not support setup')
+            end
+          end
+          adapters[#adapters + 1] = adapter
+        end
+      end
+      opts.adapters = adapters
+    end
+
+    require('neotest').setup(opts)
+  end,
   keys = {
     { '<leader>t', '', desc = '+test' },
     {
