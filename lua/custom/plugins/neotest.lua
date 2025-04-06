5f2f60e7b27ba836b1ef890100f3bb903e7a3dcd
Evan Campbell <evan.garreth@gmail.com>
2025-04-06 12:09:07 -0500

    revert lazy config & just add keys

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -8,45 +8,14 @@ return {
     'rouge8/neotest-rust',
     'nvim-neotest/nvim-nio',
   },
-  opts = {
-    status = { virtual_text = true },
-    output = { open_on_run = true },
-    adapters = {
-      'neotest-dotnet',
-      'neotest-rust',
-    },
-  },
   config = function()
-    if opts.adapters then
-      local adapters = {}
-      for name, config in pairs(opts.adapters or {}) do
-        if type(name) == 'number' then
-          if type(config) == 'string' then
-            config = require(config)
-          end
-          adapters[#adapters + 1] = config
-        elseif config ~= false then
-          local adapter = require(name)
-          if type(config) == 'table' and not vim.tbl_isempty(config) then
-            local meta = getmetatable(adapter)
-            if adapter.setup then
-              adapter.setup(config)
-            elseif adapter.adapter then
-              adapter.adapter(config)
-              adapter = adapter.adapter
-            elseif meta and meta.__call then
-              adapter = adapter(config)
-            else
-              error('Adapter ' .. name .. ' does not support setup')
-            end
-          end
-          adapters[#adapters + 1] = adapter
-        end
-      end
-      opts.adapters = adapters
-    end
-
-    require('neotest').setup(opts)
+    local neotest = require 'neotest'
+    neotest.setup {
+      adapters = {
+        require 'neotest-dotnet',
+        require 'neotest-rust',
+      },
+    }
   end,
   keys = {
     { '<leader>t', '', desc = '+test' },
